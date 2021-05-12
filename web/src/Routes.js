4425547e253395e9a335efcbb85cdd351082f906
Anisha <anisha.malde@ibm.com>
2021-05-12 17:12:54 +0100

    Add db

--- web/src/Routes.js
+++ web/src/Routes.js
@@ -19,13 +19,11 @@ const Routes = () => {
         <Route path="/about" page={AboutPage} name="about" />
         <Route path="/" page={HomePage} name="home" />
       </Set>
-      <Private unauthenticated="home">
-        <Route path="/admin/posts/new" page={NewPostPage} name="newPost" />
-        <Route path="/admin/posts/{id:Int}/edit" page={EditPostPage} name="editPost" />
-        <Route path="/admin/posts/{id:Int}" page={PostPage} name="post" />
-        <Route path="/admin/posts" page={PostsPage} name="posts" />
-        <Route notfound page={NotFoundPage} />
-      </Private>
+      <Route path="/lock/posts/new" page={NewPostPage} name="newPost" />
+      <Route path="/lock/posts/{id:Int}/edit" page={EditPostPage} name="editPost" />
+      <Route path="/lock/posts/{id:Int}" page={PostPage} name="post" />
+      <Route path="/lock/posts" page={PostsPage} name="posts" />
+      <Route notfound page={NotFoundPage} />
     </Router>
   )
 }
